dbba3909ced5ea4186269a6c204a7dac2e35485c
Dahlia1337 <vinhluongngo@gmail.com>
2025-12-05 22:01:49 +0700

    add lcd, traffic logic, output display

--- Core/Src/output_display.c
+++ Core/Src/output_display.c
@@ -4,11 +4,61 @@
  * Created on: Dec 3, 2025
  * Author: TRONG
  */
+
 #include "output_display.h"
 
-void displayLED_RED(int IS_ON, int index){}
+void displayLED_RED(int IS_ON, int index){
+    // Logic ĐỎ: A = 1, B = 1. Nếu IS_ON thì set màu theo mã, không thì tắt
+    GPIO_PinState stateA = (IS_ON == 1) ? GPIO_PIN_SET : GPIO_PIN_RESET;
+    GPIO_PinState stateB = (IS_ON == 1) ? GPIO_PIN_SET : GPIO_PIN_RESET;
 
-void displayLED_YELLOW(int IS_ON, int index){}
+    switch(index){
+        case 0: // Traffic Light 1
+            HAL_GPIO_WritePin(LIGHT_PORT, TL1_PIN_A, stateA);
+            HAL_GPIO_WritePin(LIGHT_PORT, TL1_PIN_B, stateB);
+            break;
+        case 1: // Traffic Light 2
+            HAL_GPIO_WritePin(LIGHT_PORT, TL2_PIN_A, stateA);
+            HAL_GPIO_WritePin(LIGHT_PORT, TL2_PIN_B, stateB);
+            break;
+    }
+}
 
-void displayLED_GREEN(int IS_ON, int index){}
+void displayLED_YELLOW(int IS_ON, int index){
+    // Logic VÀNG: A = 0, B = 1
+    // Nếu IS_ON = 0 (tắt) thì cả A và B đều về 0
+    GPIO_PinState stateA = GPIO_PIN_RESET;
+    GPIO_PinState stateB = (IS_ON == 1) ? GPIO_PIN_SET : GPIO_PIN_RESET;
+
+    switch(index){
+        case 0:
+            HAL_GPIO_WritePin(LIGHT_PORT, TL1_PIN_A, stateA);
+            HAL_GPIO_WritePin(LIGHT_PORT, TL1_PIN_B, stateB);
+            break;
+        case 1:
+            HAL_GPIO_WritePin(LIGHT_PORT, TL2_PIN_A, stateA);
+            HAL_GPIO_WritePin(LIGHT_PORT, TL2_PIN_B, stateB);
+            break;
+    }
+}
+
+void displayLED_GREEN(int IS_ON, int index){
+    // Logic XANH: A = 1, B = 0
+    // Nếu IS_ON = 0 (tắt) thì cả A và B đều về 0
+    GPIO_PinState stateA = (IS_ON == 1) ? GPIO_PIN_SET : GPIO_PIN_RESET;
+    GPIO_PinState stateB = GPIO_PIN_RESET;
+
+    switch(index){
+        case 0:
+            HAL_GPIO_WritePin(LIGHT_PORT, TL1_PIN_A, stateA);
+            HAL_GPIO_WritePin(LIGHT_PORT, TL1_PIN_B, stateB);
+            break;
+        case 1:
+            HAL_GPIO_WritePin(LIGHT_PORT, TL2_PIN_A, stateA);
+            HAL_GPIO_WritePin(LIGHT_PORT, TL2_PIN_B, stateB);
+            break;
+    }
+}
+
+
 
--- Core/Src/traffic_logic.c
+++ Core/Src/traffic_logic.c
@@ -1,19 +1,31 @@
-/*
- * traffic_logic.c
- *
- *  Created on: Oct 13, 2025
- *      Author: TRONG
- */
-
-
 #include "traffic_logic.h"
 
+// Giá trị khởi tạo ban đầu (Đỏ: 5s, Vàng: 2s, Xanh: 3s)
+static int duration_red_ms = 5000;
+static int duration_yellow_ms = 2000;
+static int duration_green_ms = 3000;
+
 // --- THỰC THI CÁC HÀM GETTER ---
-int getDurationTime_RED_ms(void) {}
-int getDurationTime_YELLOW_ms(void) {}
-int getDurationTime_GREEN_ms(void) {}
+int getDurationTime_RED_ms(void) {
+    return duration_red_ms;
+}
+int getDurationTime_YELLOW_ms(void) {
+    return duration_yellow_ms;
+}
+int getDurationTime_GREEN_ms(void) {
+    return duration_green_ms;
+}
 
 // --- THỰC THI CÁC HÀM SETTER ---
-void setDurationTime_RED_ms(int duration_ms) {}
-void setDurationTime_YELLOW_ms(int duration_ms) {}
-void setDurationTime_GREEN_ms(int duration_ms) {}
+void setDurationTime_RED_ms(int duration_ms) {
+    duration_red_ms = duration_ms;
+}
+void setDurationTime_YELLOW_ms(int duration_ms) {
+    duration_yellow_ms = duration_ms;
+}
+void setDurationTime_GREEN_ms(int duration_ms) {
+    duration_green_ms = duration_ms;
+}
+
+// Hàm restartAllFromScratch sẽ được thực hiện trong timer.c
+// vì nó cần truy cập vào các biến trạng thái static của timer.
