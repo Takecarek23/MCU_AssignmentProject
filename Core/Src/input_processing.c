4d0a658b742fd8e00e366aab330956e75ba5ecaf
“Takecarek23” <nguyenbaotrong18102018@gmail.com>
2025-12-07 16:11:16 +0700

    frame for fsm

--- Core/Src/input_processing.c
+++ Core/Src/input_processing.c
@@ -1,10 +1,132 @@
 /*
  * input_processing.c
  *
- * Created on: Dec 3, 2025
+ * Created on: Oct 10, 2025
  * Author: TRONG
  */
 
+#include "main.h"
+#include "i2c-lcd.h"
+#include "input_reading.h"
+#include "output_display.h"
+#include "timer.h"
+#include "traffic_logic.h"
 #include "input_processing.h"
 
-void fsm_for_input_processing(void) {}
+static enum Mode currentMode = MODE_1_NORMAL;
+static enum State prevState[NO_OF_BUTTONS] = {RELEASED, RELEASED};
+static enum State currState[NO_OF_BUTTONS] = {RELEASED, RELEASED};
+
+extern int getDurationTime_RED_ms(void);
+extern void setDurationTime_RED_ms(int duration);
+extern int getDurationTime_YELLOW_ms(void);
+extern void setDurationTime_YELLOW_ms(int duration);
+extern int getDurationTime_GREEN_ms(void);
+extern void setDurationTime_GREEN_ms(int duration);
+extern void restartAllFromScratch(void);
+
+int manualPhase = 0;
+static int temp_duration_ms = 0;
+void fsm_for_input_processing(void) {
+    // 1. Read and update the status for all buttons
+    for (int i = 0; i < NO_OF_BUTTONS; ++i) {
+        prevState[i] = currState[i];
+        if (is_button_pressed(i) == 1) {
+            currState[i] = PRESSED;
+        } else {
+            currState[i] = RELEASED;
+        }
+    }
+
+    // 2. Create a flag for each button click event to make the code easier to read
+    int is_mode_button_pressed		= (currState[0] == PRESSED && prevState[0] == RELEASED);
+    int is_increment_event       	= is_button_incrementing(1);
+
+    // 3. Main logic processing based on current mode (currentMode)
+    switch (currentMode) {
+        case MODE_1_NORMAL:
+        	flagInterruptLED_and_SEG = 1; // To turn on the normal state
+        	LCD_Show_Mode(0); // Là mode auto
+        	// Lấy giá trị từ timer.c và chia cho 100 để ra giây (vì chu kỳ là 10ms)
+			int time1_sec = getTimer1Value() / 100;
+			int time2_sec = getTimer2Value() / 100; // Sửa lỗi cũ: dùng getTimer2Value
+
+			LCD_Show_Data(0, time1_sec, time2_sec);
+            if (is_mode_button_pressed) { // If button 1 is pressed, go to edit mode RED
+                currentMode = MODE_2_MANUAL;
+                temp_duration_ms = getDurationTime_RED_ms();
+            }
+            break;
+        case MODE_2_MANUAL:
+			LCD_Show_Mode(1); // Luôn hiển thị tiêu đề hàng 1
+
+			// --- 4. CHUYỂN MODE ---
+			if (is_mode_button_pressed) {
+				currentMode = MODE_3_RED_MODIFY;
+			}
+			break;
+        case MODE_3_RED_MODIFY:
+        	LCD_Show_Mode(2);
+
+			LCD_Show_Data(2, temp_duration_ms / 1000, temp_duration_ms / 1000);
+
+            // Switch to MODE 3 if "is_mode_button_pressed"
+            if (is_mode_button_pressed) {
+                currentMode = MODE_4_YELLOW_MODIFY;
+                temp_duration_ms = getDurationTime_YELLOW_ms();
+            }
+
+            // Hold down button 2
+			if (is_increment_event) {
+				temp_duration_ms += 1000;
+				if (temp_duration_ms > 99000) temp_duration_ms = 1000;
+				setDurationTime_RED_ms(getDurationTime_GREEN_ms() + getDurationTime_YELLOW_ms());
+			}
+            break;
+
+        case MODE_4_YELLOW_MODIFY:
+        	// Hiển thị mode 3 trên dòng đầu tiên lcd
+        	LCD_Show_Mode(3);
+
+            // Hiển thị dòng 2, 2 số thời gian gian giống nhau
+            LCD_Show_Data(3, temp_duration_ms / 1000, temp_duration_ms / 1000);
+
+            // Nếu nút 1 được nhấn
+            if (is_mode_button_pressed) {
+                currentMode = MODE_5_GREEN_MODIFY; // Chuyển sang chế độ điều chỉnh đèn xanh
+                temp_duration_ms = getDurationTime_GREEN_ms(); // Lấy thời gian hiện tại của đèn xanh
+            }
+
+			if (is_increment_event) { // Nếu nút 2 được nhấn
+				temp_duration_ms += 1000;
+				if (temp_duration_ms > 99000) temp_duration_ms = 1000;
+				setDurationTime_YELLOW_ms(temp_duration_ms); // Lưu thời gian đèn vàng
+				setDurationTime_RED_ms(getDurationTime_GREEN_ms() + getDurationTime_YELLOW_ms());
+				// Tới đây đèn đỏ mới cập nhật
+			}
+            break;
+
+        case MODE_5_GREEN_MODIFY:
+        	LCD_Show_Mode(4);
+
+            LCD_Show_Data(3, temp_duration_ms / 1000, temp_duration_ms / 1000);
+
+            if (is_mode_button_pressed) {
+                currentMode = MODE_1_NORMAL;
+                restartAllFromScratch();
+                temp_duration_ms = getDurationTime_GREEN_ms();
+            }
+
+			if (is_increment_event) {
+				temp_duration_ms += 1000;
+				if (temp_duration_ms > 99000) temp_duration_ms = 1000;
+				setDurationTime_GREEN_ms(temp_duration_ms); // Lưu thời gian đèn vàng
+				setDurationTime_RED_ms(getDurationTime_GREEN_ms() + getDurationTime_YELLOW_ms());
+			}
+            break;
+
+        default:
+            currentMode = MODE_1_NORMAL;
+            break;
+    }
+}
